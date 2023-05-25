d321dfa931597c7cfa8e31d25a4d87c5c227218b
bths-ax <ardenx@nycstudents.net>
2023-05-25 08:44:23 -0400

    implement calculator and some more ui stuff

---
+++ src/Calculator.java
@@ -0,0 +1,77 @@
+import javax.swing.*;
+import java.util.ArrayList;
+
+public class Calculator extends AbstractListModel<Double> {
+    private static double EPSILON = 1e-9; // thanks ieee 754!
+
+    private ArrayList<Double> data;
+
+    public Calculator() {
+        this.data = new ArrayList<Double>();
+    }
+
+    public void add(double value) {
+        // Keep the data sorted - binary search sorting
+        int lower = 0;
+        int upper = data.size();
+        while (lower < upper) {
+            int middle = (lower + upper) / 2;
+            if (data.get(middle) > value)
+                upper = middle;
+            else
+                lower = middle + 1;
+        }
+        data.add(lower, value);
+    }
+
+    public void remove(int index) {
+        data.remove(index);
+    }
+
+    public double calculateMedian() {
+        if (data.size() == 0) return 0;
+        if (data.size() % 2 == 0) {
+            double left = data.get(data.size() / 2 - 1);
+            double right = data.get(data.size() / 2);
+            return (left + right) / 2;
+        } else {
+            return data.get(data.size() / 2);
+        }
+    }
+
+    public double calculateMean() {
+        if (data.size() == 0) return 0;
+        double sum = 0;
+        for (double num : data)
+            sum += num;
+        return sum / data.size();
+    }
+
+    public double calculateStd() {
+        if (data.size() == 0) return 0;
+        double mean = calculateMean();
+        double sum2 = 0;
+        for (double num : data)
+            sum2 += Math.pow(num - mean, 2);
+        return Math.sqrt(sum2 / data.size());
+    }
+
+    public double calculatePercentWithin(double left, double right) {
+        int count = 0;
+        for (double num : data)
+            if (left - EPSILON < num && num < right + EPSILON)
+                count++;
+        return (double)count / data.size();
+    }
+
+    public double calculatePercentWithinStd(double left, double right) {
+        double mean = calculateMean();
+        double std = calculateStd();
+        return calculatePercentWithin(mean + std * left, mean + std * right);
+    }
+
+    @Override
+    public int getSize() { return data.size(); }
+    @Override
+    public Double getElementAt(int index) { return data.get(index); }
+}
--- src/MainGUI.java
+++ src/MainGUI.java
@@ -1,2 +1,46 @@
-package PACKAGE_NAME;public class MainGUI {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import javax.swing.*;
+
+public class MainGUI extends JFrame implements ActionListener {
+    private JPanel mainPanel;
+    private JList dataList;
+    private JTextField dataField;
+    private JButton addButton;
+    private JButton removeButton;
+    private JLabel meanLabel;
+    private JPanel statPanel;
+    private JTextField lowerField;
+    private JLabel withinLabel1;
+    private JLabel withinLabel2;
+    private JLabel withinLabel;
+    private JTextField upperField;
+    private JLabel medianLabel;
+    private JLabel stdLabel;
+
+    private Calculator calculator;
+
+    public MainGUI() {
+        this.calculator = new Calculator();
+
+        createUIComponents();
+    }
+
+    public void createUIComponents() {
+        setContentPane(mainPanel);
+        setTitle("Standard Deviation Calculator");
+        setSize(750, 300);
+        setLocationByPlatform(true);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setVisible(true);
+
+        dataList.setModel(calculator);
+        addButton.addActionListener(this);
+        removeButton.addActionListener(this);
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        Object obj = e.getSource();
+    }
 }
