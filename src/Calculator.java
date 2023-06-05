c310104f24ee85b264023e8b1b4676546150187f
bths-ax <ardenx@nycstudents.net>
2023-06-05 08:22:05 -0400

    made serialization upper case bc that would be funny i think

--- src/Calculator.java
+++ src/Calculator.java
@@ -79,7 +79,7 @@ public class Calculator extends AbstractListModel<Double> {
     public String serialize() {
         String out = "std:";
         for (double value : data)
-            out += String.format("%x", Double.doubleToLongBits(value));
+            out += String.format("%X", Double.doubleToLongBits(value));
         return out;
     }
 
